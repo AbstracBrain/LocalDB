8412b5650a6a61e24d395d7b87075ab54939bce7
Alumno <Alumno@PC33>
2019-11-13 12:34:07 -0600

    LISTO

---
+++ LocalDB/LocalDB/MainWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Data;
+using System.Data.SqlClient; //Agregar libreria
+namespace LocalDB
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        SqlConnection conn = new SqlConnection("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\Alumno\\source\\repos\\LocalDB\\LocalDB\\LocalDB\\MiBaseDatos.mdf;Integrated Security=True");
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
